dbfd786f54f336ccb5668d6d7c95deed447b9e15
0x63s <stefan.ilic@hotmail.ch>
2025-11-01 12:40:52 +0100

    Refactor Submission ID generation strategy to use UUID and migrate application properties to MySQL

--- code/backend/src/main/java/ch/baselhack/underwriting/model/Submission.java
+++ code/backend/src/main/java/ch/baselhack/underwriting/model/Submission.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 @Table(name = "submission")
 public class Submission {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @NotNull
