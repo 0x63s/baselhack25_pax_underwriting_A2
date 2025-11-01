2c1b94b416201498c5aea0c194fc30e4fb57bc81
Milan Balabanovic <miki.balabanovic111@gmail.com>
2025-11-01 08:56:50 +0100

    added questions to backend

--- code/backend/src/main/java/ch/baselhack/underwriting/resources/constants/ApplicationConstants.java
+++ code/backend/src/main/java/ch/baselhack/underwriting/resources/constants/ApplicationConstants.java
@@ -8,4 +8,5 @@ public class ApplicationConstants {
     public static final String API_V1 = "/api/v1";
     public static final String OFFERINGS_API = API_V1 + "/offerings";
     public static final String CLIENTS_API = API_V1 + "/clients";
+    public static final String QUESTIONS_API = API_V1 + "/questions";
 }
