f3c0dfb22847a11a7770691ef59e623c3fe1cb9b
0x63s <stefan.ilic@hotmail.ch>
2025-11-02 11:05:05 +0100

    Removed old data, fixed App.js

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import axios from 'axios';
 import './App.css';
 
 // Admin Pages
@@ -14,37 +13,7 @@ import Clients from './pages/admin/Clients';
 import NewApplicationForm from './pages/user/NewApplicationForm';
 import SuccessPage from './pages/user/SuccessPage';
 
-// Admin Pages
-import AdminDashboard from './pages/admin/AdminDashboard';
-import Offerings from './pages/admin/Offerings';
-import CreateOffering from './pages/admin/CreateOffering';
-import Questions from './pages/admin/Questions';
-import Clients from './pages/admin/Clients';
-
-// User Pages
-import NewApplicationForm from './pages/user/NewApplicationForm';
-import SuccessPage from './pages/user/SuccessPage';
-
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* Redirect root to user home */}
-          <Route path="/" element={<Navigate to="/user" replace />} />
-
-          {/* User Routes */}
-          <Route path="/user" element={<NewApplicationForm />} />
-          <Route path="/user/success" element={<SuccessPage />} />
-
-          {/* Admin Routes */}
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/admin/offerings" element={<Offerings />} />
-          <Route path="/admin/offerings/new" element={<CreateOffering />} />
-          <Route path="/admin/questions" element={<Questions />} />
-          <Route path="/admin/clients" element={<Clients />} />
-          <Route path="/admin/submissions" element={<Clients />} />
-
   return (
     <Router>
       <div className="App">
